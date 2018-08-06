5748fe6ac80894a656f1006114b98e304609bd23
Brady Clark <bradyjclark88@gmail.com>
2018-08-06 16:45:17 -0300

    add basic routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :companies, only: %i[index show] do
+    resources :reviews, only: %i[index show new create]
+  end
 end
+
+  # resources :pets do
+  #   resources :bookings, except: %i[index destroy] do
+  #     resources :reviews, except: %i[index destroy]
+  #   end
+  # end
