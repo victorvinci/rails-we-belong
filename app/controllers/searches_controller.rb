61cf0e15669939b22fc542df6235b1d35bef4107
Erika Kino <o.erika.o@gmail.com>
2018-08-08 20:19:06 -0300

    Add company model to search results

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,12 +2,16 @@ class SearchesController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
 
   def index
+    @companies = Company
+      .search(params[:query])
+      .with_pg_search_highlight
+      .page(params[:page])
     @results = Review
       .search(params[:query])
       .with_pg_search_highlight
       .page(params[:page])
     if @results.empty?
-      @companies = Company.search(params[:query]).with_pg_search_highlight.page(params[:page])
+      @companies
     end
   end
 
