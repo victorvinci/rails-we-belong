aa87c2c9dbc6b8432e30ba0e103a9c99bbec3fc7
Pedro Cezar <pedrofcezar@gmail.com>
2018-08-08 12:50:51 -0300

    make the navbar transparent in home page

--- app/javascript/components/_navbar_home.js
+++ app/javascript/components/_navbar_home.js
@@ -2,16 +2,16 @@ function initUpdateNavbarOnScroll() {
 
   const navbar = document.querySelector('.navbar-wb');
 
-    if (navbar && document.querySelector('.carousel slide')) {
+    if (navbar && document.querySelector('.home-banner')) {
       window.addEventListener('scroll', () => {
         if (window.scrollY >= window.innerHeight-400) {
-          navbar.classList.add('navbar-wagon-white');
+          navbar.classList.add('navbar-wb-home');
         } else {
-          navbar.classList.remove('navbar-wagon-white');
+          navbar.classList.remove('navbar-wb-home');
         }
       })}
     else {
-        navbar.classList.add('navbar-wagon-pages');
+        navbar.classList.add('navbar-wb-pages');
       };
     }
 
