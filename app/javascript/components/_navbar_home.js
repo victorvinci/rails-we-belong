de002256ac9230cee45915c889f6a5faeaf0ca3b
Pedro Cezar <pedrofcezar@gmail.com>
2018-08-08 20:07:46 -0300

    searchbar navibars in pages

--- app/javascript/components/_navbar_home.js
+++ app/javascript/components/_navbar_home.js
@@ -14,6 +14,7 @@ function initUpdateNavbarOnScroll() {
       })}
     else {
         navbar.classList.add('navbar-wb-pages');
+        document.getElementById('custom-search-input').classList.remove('hide');
       };
     }
 
