5ba6fde407eed825a4d9b58478bd7aec67723a07
victor vinci <victorvinci@protonmail.com>
2018-08-10 14:34:25 -0300

    fix layout /companies + add js to load more pages

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,8 @@
 import "bootstrap";
 import { initUpdateNavbarOnScroll } from '../components/_navbar_home';
-
+import { showMore } from '../components/_showMore'
 
 
 $('[data-toggle="tooltip"]').tooltip()
 initUpdateNavbarOnScroll();
+showMore();
