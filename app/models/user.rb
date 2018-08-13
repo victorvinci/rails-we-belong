33a3abacbc6bc21a23df04cfe508f87bb3923cf7
Pedro Cezar <pedrofcezar@gmail.com>
2018-08-13 18:39:02 -0300

    implement personal info in sign up page

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,16 +8,20 @@ class User < ApplicationRecord
   has_many :reviews
 
   has_one :employee_profile
+  accepts_nested_attributes_for :employee_profile
 
   validates :role, inclusion: { in: %w(employee representative),
     message: "%{value} is not a valid role type" }, allow_nil: true
 
   after_create :send_welcome_email
 
-    private
+  def has_profile?
+    !employee_profile.nil?
+  end
+
+  private
 
   def send_welcome_email
     UserMailer.welcome(self).deliver_now
   end
-
 end
