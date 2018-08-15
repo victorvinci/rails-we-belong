8688fda1fb8ff94b7ed39c6d954439f549de23a5
Erika Kino <o.erika.o@gmail.com>
2018-08-15 11:26:52 -0300

    change new user redirect to edit profile

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,25 +2,25 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def linkedin
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    if @user.persisted?
-      sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, kind: "LinkedIn") if is_navigational_format?
-    else
-      session["devise.linkedin_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
+    if @user.new_record?
+      @user.save
+      session[:user_return_to] = edit_user_registration_url
     end
+    sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
+    set_flash_message(:notice, :success, kind: "LinkedIn") if is_navigational_format?
   end
 
-  def facebook
-    @user = User.from_omniauth(request.env["omniauth.auth"])
-    if @user.persisted?
-      sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
-    else
-      session["devise.facebook_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
-    end
-  end
+  # def facebook
+  #   @user = User.from_omniauth(request.env["omniauth.auth"])
+  #   if @user.persisted?
+  #     sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
+  #     set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
+  #   else
+  #     @user.save
+  #     session["devise.facebook_data"] = request.env["omniauth.auth"]
+  #     redirect_to edit_user_registration_url
+  #   end
+  # end
 
   def failure
     redirect_to root_path
--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,7 @@ class User < ApplicationRecord
   after_create :send_welcome_email
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    where(provider: auth.provider, uid: auth.uid).first_or_initialize do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
     end
