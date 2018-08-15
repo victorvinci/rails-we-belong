ac1f983905fef328d34b9617baf4b2ab7b701942
Pedro Cezar <pedrofcezar@gmail.com>
2018-08-15 11:14:29 -0300

    cards home page and some other minor changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,13 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[home about disclaimer]
 
   def home
+    @companies = Company.all.sample(3)
   end
 
   def about
+  end
+
+  def disclaimer
   end
 
 end
