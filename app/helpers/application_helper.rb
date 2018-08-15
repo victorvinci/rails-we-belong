e4519339738c218976dcdba15d21df35b3d29505
Pedro Cezar <pedrofcezar@gmail.com>
2018-08-15 12:15:13 -0300

    navbar home1

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,5 @@
 module ApplicationHelper
-  def home_class_for_navbar
-    output = 'navbar-wb-item navbar-wb-link'
+  def home_class_for_navbar(output)
     if current_page?(root_path)
       output << ' navbar-wb-link-hiden'
     end
--- app/javascript/components/_navbar_home.js
+++ app/javascript/components/_navbar_home.js
@@ -2,11 +2,13 @@
 function initUpdateNavbarOnScroll() {
 
   const navbar = document.querySelector('.navbar-wb');
+  const profileIcon = document.querySelector('#navbar-wagon-menu');
 
     if (navbar && document.querySelector('.home-banner')) {
       window.addEventListener('scroll', () => {
         if (window.scrollY >= window.innerHeight-260) {
           navbar.classList.add('navbar-wb-home');
+          profileIcon.classList.remove('navbar-wb-link-hiden')
           document.getElementById('custom-search-input').classList.remove('hide');
           document.getElementById('linkone').classList.remove('navbar-wb-link-hiden');
           document.getElementById('linkone').classList.add('navbar-wb-link');
@@ -15,6 +17,7 @@ function initUpdateNavbarOnScroll() {
 
         } else {
           navbar.classList.remove('navbar-wb-home');
+          profileIcon.classList.add('navbar-wb-link-hiden')
           document.getElementById('custom-search-input').classList.add('hide');
           document.getElementById('linkone').classList.remove('navbar-wb-link');
           document.getElementById('linkone').classList.add('navbar-wb-link-hiden');
